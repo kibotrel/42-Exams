ad42f0a3fc4b96330bac37fb958cd2bb1efd434d
Kilian BOTREL <kibotrel@e2r12p18.42.fr>
2019-01-11 17:35:11 +0100

    Lv02 - Modification : Removed variables to go through the strings

--- Lv02/wdmatch/wdmatch.c
+++ Lv02/wdmatch/wdmatch.c
@@ -12,34 +12,27 @@ int	ft_strlen(char *str)
 
 int	wdmatch(char *s1, char *s2)
 {
-	int i;
-	int j;
+	int length;
 	int	count;
 
-	i = 0;
-	j = 0;
+	length = ft_strlen(s1);
 	count = 0;
-	while (s1[i] && s2[j])
+	while (*s1 && *s2)
 	{
-		if (s1[i] == s2[j])
+		if (*s1 == *s2++)
 		{
-			i++;
-			j++;
+			s1++;
 			count++;
 		}
-		else
-			j++;
 	}
-	return ((count == ft_strlen(s1) ? 1 : 0));
+	return ((count == length ? 1 : 0));
 }
 
 int	main(int ac, char **av)
 {
 	if (ac == 3)
-	{
 		if (wdmatch(av[1], av[2]))
-				write (1, av[1], ft_strlen(av[1]));
-	}
-	write (1, "\n", 1);
+				write(1, av[1], ft_strlen(av[1]));
+	write(1, "\n", 1);
 	return (0);
 }
