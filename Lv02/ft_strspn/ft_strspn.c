c30d441f2acbc77b01f108c7d99b9dd2edc5b4bf
Kilian BOTREL <kibotrel@e2r10p3.42.fr>
2019-02-04 21:12:59 +0100

    Lv02 - New Exercice - ft_strspn

---
+++ Lv02/ft_strspn/ft_strspn.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strspn.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/25 03:10:53 by kibotrel          #+#    #+#             */
+/*   Updated: 2019/01/25 03:30:53 by kibotrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		check_char(char c, const char *charset)
+{
+	int		i;
+
+	i = -1;
+	while (charset[++i])
+		if (charset[i] == c)
+			return (1);
+	return (0);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+	size_t	i;
+
+	i = 0;
+
+	while (s[i])
+	{
+		if(!check_char(s[i], accept))
+			return (i);
+		i++;
+	}
+	return (i);
+}
