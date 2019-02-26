3692fa9aeb66fde7eacf1386597730991fb30101
Kilian BOTREL <kibotrel@e1r10p3.42.fr>
2019-02-26 09:44:15 +0100

    Lv04 - Norm - moment

--- Lv04/moment/moment.c
+++ Lv04/moment/moment.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moment.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kibotrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: kibotrel <kibotrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 16:21:28 by kibotrel          #+#    #+#             */
-/*   Updated: 2019/01/10 12:45:43 by kibotrel         ###   ########.fr       */
+/*   Updated: 2019/02/26 09:43:51 by kibotrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #define DAY 86400
 #define MONTH 2592000
 
-int				ft_strlen(char *str)
+int				len(char *str)
 {
 	int				i;
 
@@ -52,35 +52,37 @@ char			*small_itoa(unsigned int nb)
 
 char			*ft_strjoin(char *s1, char *s2)
 {
-
 	int				i;
 	char			*time;
 
 	i = 0;
-	if (!(time = (char*)malloc(sizeof(*time) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if (!(time = (char*)malloc(sizeof(*time) * (len(s1) + len(s2) + 1))))
 		return (NULL);
 	while (*s1)
 		time[i++] = *s1++;
 	while (*s2)
-		time[i++] = *s2++;;
+		time[i++] = *s2++;
 	time[i] = '\0';
 	return (time);
 }
 
 char			*moment(unsigned int duration)
 {
-	char			*time;
-	char			*value;
-
 	if (duration >= MONTH)
-		return (time = ((duration / MONTH) <= 1 ? "1 month ago." : ft_strjoin(small_itoa(duration / MONTH), " months ago.")));
+		return (((duration / MONTH) <= 1 ? "1 month ago."
+			: ft_strjoin(small_itoa(duration / MONTH), " months ago.")));
 	else if (duration >= DAY)
-		return (time = ((duration / DAY) <= 1 ? "1 day ago." : ft_strjoin(small_itoa(duration / DAY), " days ago.")));
+		return (((duration / DAY) <= 1 ? "1 day ago."
+			: ft_strjoin(small_itoa(duration / DAY), " days ago.")));
 	else if (duration >= HOUR)
-		return (time = ((duration / HOUR) <= 1 ? "1 hour ago." : ft_strjoin(small_itoa(duration / HOUR), " hours ago.")));
+		return (((duration / HOUR) <= 1 ? "1 hour ago."
+			: ft_strjoin(small_itoa(duration / HOUR), " hours ago.")));
 	else if (duration >= MINUTE)
-		return (time = ((duration / MINUTE) <= 1 ? "1 minute ago." : ft_strjoin(small_itoa(duration / MINUTE), " mintutes ago.")));
+		return (((duration / MINUTE) <= 1 ? "1 minute ago."
+			: ft_strjoin(small_itoa(duration / MINUTE), " mintutes ago.")));
 	else if (duration >= SECOND)
-		return (time = ((duration / SECOND) <= 1 ? "1 second ago." : ft_strjoin(small_itoa(duration / SECOND), " seconds ago.")));
-	return (time = "0 seconds ago.");
+		return (((duration / SECOND) <= 1 ? "1 second ago."
+			: ft_strjoin(small_itoa(duration / SECOND), " seconds ago.")));
+	else
+		return ("0 seconds ago.");
 }
